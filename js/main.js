11aafac1f72a682a70c2936de08b3ae59101cfb8
Eric Kudler <joeento@gmail.com>
2014-06-09 15:19:24 -0400

    Board will now be painted on load using js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+(function(jQuery) {
+	var Board = function(width, height) {
+		this.width = width;
+		this.height = height;
+		this.initBoard = function() {
+			for (var h=0;h<this.height;h++) {
+				var tableRow = '<tr>'+(new Array(width + 1).join('<td><div class="box"></div></td>'))+'</tr>';
+				jQuery(".box-container table").append(tableRow);
 
+			}
+		}
+	};
+
+	var board = new Board(12,15);
+	board.initBoard();
+})($);
